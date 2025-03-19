019ce66e2de8af46caef11f6aa5fd85105ba507a
Shikhar Bhandari <shikhar.bhandari@cbre.com>
2025-03-19 06:03:30 +0530

    not found issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [
     checker({ typescript: false }),
     react(),
@@ -23,7 +23,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: 'dist',
     emptyOutDir: true,
   },
 });
